9a638882630df2d575b02c4aea0e4c1014977d18
Misha <demusmachine@gmail.com>
2026-06-25 23:55:08 +0800

    changes

--- src/components/ThankYou.tsx
+++ src/components/ThankYou.tsx
@@ -1,4 +1,7 @@
 export default function ThankYou() {
+  const handleCompleteStudy = () => {
+  window.location.href = 'https://app.prolific.com/submissions/complete?cc=CALS53S3';
+  };
   return (
     <div className="card">
       <h1>Thank you!</h1>
@@ -6,6 +9,9 @@ export default function ThankYou() {
         Your responses have been recorded.
       </p>
       <p>You may now close this window and return to Prolific.</p>
+        <button onClick={handleCompleteStudy}>
+          Return to Prolific
+        </button>
     </div>
   )
 }
